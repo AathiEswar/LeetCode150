77bc0dc21ee4464558efb13c770383b4a6e7b66a
AathiEswar <aathi28082003@gmail.com>
2023-12-17 10:31:32 +0530

    17-12-2023

---
+++ BinarySearchTree.java
@@ -0,0 +1,31 @@
+package LeetCode150;
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+}
+
+public class BinarySearchTree {
+    TreeNode prev = null;
+    int min = Integer.MAX_VALUE;
+
+    public void inOrder(TreeNode root){
+        if(root == null)return;
+
+        inOrder(root.left);
+
+        if(prev!=null){
+            min = Math.min(min, root.val - prev.val);
+        }
+        prev = root;
+
+        inOrder(root.right);
+    }
+
+    public int getMinimumDifference(TreeNode root) {
+        inOrder(root);
+        return min;
+    }
+}
--- HashMap/IsIsomorphic.java
+++ HashMap/IsIsomorphic.java
@@ -7,9 +7,6 @@ public class IsIsomorphic {
         public boolean isIsomorphic(String s, String t) {
             int[] map1 = new int[200];
             int[] map2 = new int[200];
-
-
-
             for(int i=0;i<s.length();i++){
                 if(map1[s.charAt(i)] != map2[t.charAt(i)]){
                     return false;
