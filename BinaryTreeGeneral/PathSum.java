ca91dc8e30f059100cf7627be3c72b104d2ec234
AathiEswar <aathi28082003@gmail.com>
2024-01-21 10:42:13 +0530

    21-1-2024

---
+++ BinaryTreeGeneral/PathSum.java
@@ -0,0 +1,18 @@
+package LeetCode150.BinaryTreeGeneral;
+
+public class PathSum {
+    public boolean hasPathSum(TreeNode root , int targetSum){
+        if(root == null){
+            return false ;
+        }
+        boolean left = hasPathSum(root.left , targetSum-root.val);
+
+        if(root.left == null && root.right == null){
+            return targetSum == root.val;
+        }
+
+        boolean right = hasPathSum(root.right , targetSum-root.val);
+
+        return left || right;
+    }
+}
