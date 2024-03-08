3ae4ed46bf0df62a7efa72269975f46f48b547b4
AathiEswar <aathi28082003@gmail.com>
2024-03-08 20:48:50 +0530

    8-3-2024

--- ArraysAndString/InsertDeleteRandom.java
+++ ArraysAndString/InsertDeleteRandom.java
@@ -1,5 +1,5 @@
 package LeetCode150.ArraysAndString;
-import java.util.HashSet;
+import java.util.*;
 
 public class InsertDeleteRandom {
 
@@ -34,3 +34,45 @@ public class InsertDeleteRandom {
         return (int)(set.toArray()[random]);
     }
 }
+
+
+
+class RandomizedSet {
+    private ArrayList<Integer> list;
+    private Map<Integer, Integer> map;
+
+    public RandomizedSet() {
+        list = new ArrayList<>();
+        map = new HashMap<>();
+    }
+
+    public boolean search(int val) {
+        return map.containsKey(val);
+    }
+
+    public boolean insert(int val) {
+        if (search(val)) return false;
+
+        list.add(val);
+        map.put(val, list.size() - 1);
+        return true;
+    }
+
+    public boolean remove(int val) {
+        if (!search(val)) return false;
+
+        int index = map.get(val);
+        list.set(index, list.get(list.size() - 1));
+        map.put(list.get(index), index);
+        list.remove(list.size() - 1);
+        map.remove(val);
+
+        return true;
+    }
+
+    public int getRandom() {
+        Random rand = new Random();
+        return list.get(rand.nextInt(list.size()));
+    }
+}
+
