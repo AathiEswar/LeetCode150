1c7c382a3de33fb489f6eefed122d4482f3b4598
AathiEswar <aathi28082003@gmail.com>
2024-04-03 09:21:43 +0530

    3-4-2024

--- LinkedList/RemoveNthFromLast.java
+++ LinkedList/RemoveNthFromLast.java
@@ -24,4 +24,28 @@ public class RemoveNthFromLast {
 
         return head;
     }
+
+    //ONe pass
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode left = dummy;
+        ListNode right = head;
+
+        while(n > 0 && right != null){
+            right = right.next;
+            n-=1;
+        }
+
+        while(right != null){
+            left = left.next;
+            right = right.next;
+        }
+
+        left.next = left.next.next;
+
+        return dummy.next;
+
+    }
 }
