49df72c05950dca9f21a8b59ade1c2683374af34
AathiEswar <aathi28082003@gmail.com>
2024-06-27 07:32:52 +0530

    27-6-2024

--- ArraysAndString/RomanToInteger.java
+++ ArraysAndString/RomanToInteger.java
@@ -3,7 +3,27 @@ package LeetCode150.ArraysAndString;
 import java.util.HashMap;
 
 public class RomanToInteger {
-    public int romanToInt(String s) {
+    public int romaonToInt2(String s){
+        int ans = 0;
+        int num = 0;
+
+        for(int i = s.length()-1 ; i>=0 ; i--){
+            switch (s.charAt(i)){
+                case 'I' : num = 1; break;
+                case 'V' : num = 5; break;
+                case 'X' : num = 10; break;
+                case 'L' : num = 50; break;
+                case 'C' : num = 100; break;
+                case 'D' : num = 500; break;
+                case 'M' : num = 1000; break;
+            }
+            if(4 * num < ans) ans-= num;
+            else ans += num;
+        }
+
+        return ans;
+    }
+    public int romanToInt1(String s) {
         HashMap<Character , Integer> m = new HashMap<>();
         m.put('I', 1);
         m.put('V', 5);
